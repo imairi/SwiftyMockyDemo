56292c9f7d8dad7f5c07dc3118afe237d8e55b8e
imairi <imairi@gmail.com>
2019-09-21 13:02:34 +0900

    Initial Commit

---
+++ SwiftyMockyDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftyMockyDemo
+//
+//  Created by 今入　庸介 on 2019/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
